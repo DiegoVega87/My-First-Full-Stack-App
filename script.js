08bea103a56ba8f514254d71bd7004b5ac8d230d
DiegoVega87 <diegovega@me.com>
2023-12-23 12:17:43 +0100

    Add vote count and fact age calculations

--- script.js
+++ script.js
@@ -12,3 +12,30 @@ btn.addEventListener("click", function () {
     btn.textContent = "Share a Fact";
   }
 });
+
+let votesInteresting = 23;
+let votesMindBlowing = 5;
+const text = "Lisbon is the capital of Portugal";
+
+votesInteresting = votesInteresting + 1;
+votesInteresting++;
+console.log(votesInteresting);
+
+let totalUpvotes = votesInteresting + votesMindBlowing;
+console.log("UpVotes:", totalUpvotes);
+
+let votesFalse = 4;
+const isCorrect = votesFalse < totalUpvotes;
+console.log("Is correct:", isCorrect);
+
+function calcFactAge(year) {
+  const currentYear = new Date().getFullYear();
+  // 2023 - 2015
+  const age = currentYear - year;
+  return age;
+}
+
+const age1 = calcFactAge(2015);
+console.log(age1);
+console.log(calcFactAge(2020));
+console.log(calcFactAge(1990));
