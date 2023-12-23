3eec04b1692369a9ea2471c11f646fb3e0ecef2b
DiegoVega87 <diegovega@me.com>
2023-12-23 12:39:30 +0100

    Added use examples of taking decisions with if/else statements

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("Hello World!");
-
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
 
@@ -27,16 +25,36 @@ console.log("UpVotes:", totalUpvotes);
 let votesFalse = 4;
 const isCorrect = votesFalse < totalUpvotes;
 console.log("Is correct:", isCorrect);
+*/
 
 function calcFactAge(year) {
   const currentYear = new Date().getFullYear();
   // 2023 - 2015
   const age = currentYear - year;
-  return age;
+  if (age >= 0) return age;
+  else return "Impossible year!";
 }
 
 const age1 = calcFactAge(2015);
 console.log(age1);
 console.log(calcFactAge(2020));
 console.log(calcFactAge(1990));
-*/
+console.log(calcFactAge(2037));
+
+let votesInteresting = 20;
+let votesMindBlowing = 0;
+
+if (votesInteresting === votesMindBlowing) {
+  alert("This fact is equally interesting and mindblowing");
+} else if (votesInteresting > votesMindBlowing) {
+  console.log("Interesting Fact!");
+} else if (votesInteresting < votesMindBlowing) {
+  console.log("Mindblowing Fact!");
+}
+// falsy values: 0, "", undefined, null, undefined
+// truthy values: everything else
+if (votesMindBlowing) {
+  console.log("Mindblowing Fact!");
+} else {
+  console.log("Not so special...");
+}
