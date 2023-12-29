4b4f078f7c6362adc8450c0d00443e03819faa62
DiegoVega87 <diegovega@me.com>
2023-12-29 17:14:35 +0100

    Refactor FactList component and update max-width in style.css

--- src/App.js
+++ src/App.js
@@ -87,35 +87,38 @@ function FactList() {
     <section>
       <ul className="facts-list">
         {facts.map((fact) => (
-          <li className="fact">
-            <p>{fact.text}</p>
-            <a
-              className="source"
-              href={fact.source}
-              target="_blank"
-              rel="noreferrer"
-            >
-              (Source)
-            </a>
-            <span
-              className="tag"
-              style={{
-                backgroundColor: CATEGORIES.find(
-                  (cat) => cat.name === fact.category
-                ).color,
-              }}
-            >
-              {fact.category}
-            </span>
-            <div className="vote-buttons">
-              <button>👍 {fact.votesInteresting}</button>
-              <button>🤯 {fact.votesMindblowing}</button>
-              <button>⛔ {fact.votesFalse}</button>
-            </div>
-          </li>
+          <Fact key={fact.id} factObj={fact} />
         ))}
       </ul>
+      <p>There are {facts.length} facts in the database. Add your own!</p>
     </section>
+  );
+}
+
+function Fact({ factObj, test }) {
+  const fact = factObj;
+
+  return (
+    <li className="fact">
+      <p>{fact.text}</p>
+      <a className="source" href={fact.source} target="_blank" rel="noreferrer">
+        (Source)
+      </a>
+      <span
+        className="tag"
+        style={{
+          backgroundColor: CATEGORIES.find((cat) => cat.name === fact.category)
+            .color,
+        }}
+      >
+        {fact.category}
+      </span>
+      <div className="vote-buttons">
+        <button>👍 {fact.votesInteresting}</button>
+        <button>🤯 {fact.votesMindblowing}</button>
+        <button>⛔ {fact.votesFalse}</button>
+      </div>
+    </li>
   );
 }
 export default App;
